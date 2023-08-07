47a2afcde1811981ef269d8aff8825b206bb826a
JaeYong <yjy850146@gmail.com>
2023-08-07 14:36:04 +0900

    최초 커밋

---
+++ src/test/java/com/rakugaki/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakugaki.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
